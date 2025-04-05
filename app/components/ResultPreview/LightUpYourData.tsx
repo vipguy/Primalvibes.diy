4c4eeb2859801ef2033097a714597b921fc9b34e
J Chris Anderson <jchris@fireproof.storage>
2025-04-05 09:35:22 -0700

    Update image sources in LightUpYourData component for improved branding, adding dark mode support with conditional rendering of logo images.

--- app/components/ResultPreview/LightUpYourData.tsx
+++ app/components/ResultPreview/LightUpYourData.tsx
@@ -1,7 +1,8 @@
 const LightUpYourData = () => {
   return (
     <div className="flex h-full w-full items-center justify-center">
-      <img src="/code-things.png" alt="You can just code things" className="logo-pulse opacity-3" />
+      <img src="/code-things-black.png" alt="You can just code things" className="logo-pulse opacity-3 dark:hidden" />
+      <img src="/code-things.png" alt="You can just code things" className="logo-pulse opacity-3 hidden dark:block" />
     </div>
   );
 };
