c406484e6b693b3509302e6c6a8dab5d55b5ef38
J Chris Anderson <jchris@fireproof.storage>
2025-04-23 21:06:11 -0700

    Update share icon to upload icon with arrow pointing upward

--- app/components/HeaderContent/SvgIcons.tsx
+++ app/components/HeaderContent/SvgIcons.tsx
@@ -222,7 +222,13 @@ export const PublishIcon: React.FC<SvgIconProps> = ({ className = 'h-5 w-5' }) =
         strokeLinecap="round"
         strokeLinejoin="round"
         strokeWidth={1.5}
-        d="M8.684 13.342C8.886 12.938 9 12.482 9 12c0-.482-.114-.938-.316-1.342m0 2.684a3 3 0 110-2.684m0 2.684l6.632 3.316m-6.632-6l6.632-3.316m0 0a3 3 0 105.367-2.684 3 3 0 00-5.367 2.684zm0 9.316a3 3 0 105.368 2.684 3 3 0 00-5.368-2.684z"
+        d="M7 10h2m6 0h2a2 2 0 012 2v7a2 2 0 01-2 2H7a2 2 0 01-2-2v-7a2 2 0 012-2"
+      />
+      <path
+        strokeLinecap="round"
+        strokeLinejoin="round"
+        strokeWidth={1.5}
+        d="M12 16V2m0 0l-4 4m4-4l4 4"
       />
     </svg>
   );
