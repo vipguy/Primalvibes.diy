bac05ab5a37b6fff2652d479b81afca27a6c7d9b
Meno Abels <meno.abels@adviser.com>
2025-08-19 15:58:58 +0200

    chore: try to fix missing root.tsx

--- vibes.diy/tests/vitest.config.ts
+++ vibes.diy/tests/vitest.config.ts
@@ -1,10 +1,16 @@
 import { defineConfig } from "vitest/config";
 // import react from '@vitejs/plugin-react';
 import tsconfigPaths from "vite-tsconfig-paths";
+import typescript from "@rollup/plugin-typescript";
 
 export default defineConfig({
   plugins: [
-    tsconfigPaths(),
+    //typescript({
+    //  tsconfig: './tsconfig.test.json'
+    //}),
+    tsconfigPaths({
+      configNames: ["tsconfig.test.json"],
+    }),
     //    react()
   ],
   test: {
